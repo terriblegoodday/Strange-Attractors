66df09dc7767053be4f685ab6a010f0702d161a1
Eduard Dzhumagaliev <terriblegoodday@icloud.com>
2021-08-16 00:01:44 +1000

    Add Particle struct

---
+++ Strange Attractors in Metal/Particle.swift
@@ -0,0 +1,13 @@
+//
+//  Particle.swift
+//  Strange Attractors in Metal
+//
+//  Created by Eduard Dzhumagaliev on 16.08.2021.
+//
+
+import Foundation
+
+struct Particle {
+    var position: SIMD2<Float>
+    var velocity: SIMD2<Float>
+}
