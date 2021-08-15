1b8a84cad5494b875c51c878ed4b45e5af846ef9
Eduard Dzhumagaliev <terriblegoodday@icloud.com>
2021-08-15 22:41:37 +1000

    Project init

---
+++ Strange Attractors in Metal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strange Attractors in Metal
+//
+//  Created by Eduard Dzhumagaliev on 15.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
